8dc371e70da66bcbb1b31aa9a538ec03cc0a88f2
francisco2016 <otrayama@gmail.com>
2016-06-11 12:05:49 +0200

    - 0027 add atributo r24horas y modificado getTime() para poder elegir formato am/pm o 24Horas.

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -9,26 +9,31 @@ public class ClockDisplay
     private NumberDisplay2 horas;
     private NumberDisplay2 minutos;
     private String horaActual;
+    private boolean r24horas;
 
     /**
      * Constructor sin parámetros, que fija la hora a "00:00" 
+     * Si r24horas = false muestra el reloj en formato am/pm, si = true, lo muestra en formato 24 horas.
      */
-    public ClockDisplay(){
+    public ClockDisplay(boolean r24horas){
         horas = new  NumberDisplay2(24);
         horas.setValue(0);
         minutos = new  NumberDisplay2(60);
         minutos.setValue(0);
         horaActual = horas.getDisplayValue()+ " : " +minutos.getDisplayValue();
+        this.r24horas = r24horas;
     }
 
     /**
      * Constructor con 2 parámetros de tipo int, que representan las horas y los minutos a los que fijar la hora actual. 
+     * Si r24horas = false muestra el reloj en formato am/pm, si = true, lo muestra en formato 24 horas.
      */
-    public ClockDisplay(int h, int m)
+    public ClockDisplay(int h, int m, boolean r24horas)
     {
         horas = new  NumberDisplay2(h);
         minutos = new  NumberDisplay2(m);
         horaActual = horas.getDisplayValue()+ " : " +minutos.getDisplayValue();
+        this.r24horas = r24horas;
     }
 
     /**
@@ -40,29 +45,26 @@ public class ClockDisplay
         minutos.setValue(m);
     }
 
-    //     /**
-    //      * método  que devuelva la hora como String de 5 caracteres.
-    //      */
-    //     public String getTime(){
-    //         return  horas.getDisplayValue()+ " : " +minutos.getDisplayValue();
-    //     }
-    
     /**
      * método  que devuelva la hora como String de 5 caracteres.
      */
     public String getTime(){
-        
-        if(horas.getValue() > 12){
-            horaActual = (horas.getValue()-12)+ " : " +minutos.getDisplayValue()+ " p.m.";
+        if(r24horas == false){
+            if(horas.getValue() > 12){
+                horaActual = (horas.getValue()-12)+ " : " +minutos.getDisplayValue()+ " p.m.";
+            }
+            if(horas.getValue() < 12){
+                horaActual =  horas.getValue() + " : " +minutos.getDisplayValue()+ " a.m.";
+            }
+            if(horas.getValue() == 12){
+                horaActual = (horas.getValue())+ " : " +minutos.getDisplayValue()+ " p.m.";
+            }
+            if(horas.getValue() == 0){
+                horaActual = (horas.getValue() )+ " : " +minutos.getDisplayValue()+ " a.m.";
+            }
         }
-        if(horas.getValue() < 12){
-            horaActual =  horas.getValue() + " : " +minutos.getDisplayValue()+ " a.m.";
-        }
-          if(horas.getValue() == 12){
-            horaActual = (horas.getValue())+ " : " +minutos.getDisplayValue()+ " p.m.";
-        }
-          if(horas.getValue() == 0){
-            horaActual = (horas.getValue() )+ " : " +minutos.getDisplayValue()+ " a.m.";
+        else{
+            horaActual = horas.getDisplayValue()+ " : " +minutos.getDisplayValue();
         }
         return  horaActual;
     }
@@ -78,6 +80,7 @@ public class ClockDisplay
         getTime();
     }
 }
+
 
 
 
